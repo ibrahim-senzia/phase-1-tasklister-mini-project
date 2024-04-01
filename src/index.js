eb04c616e16d335158fa3a7a1ce98341bd520b2d
ibrahim senzia <ibrahimsenzia@gmail.com>
2024-04-01 16:07:18 +0300

    mini-project completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  let todoList = document.querySelector("form");
+  todoList.addEventListener("submit", function(e) {
+    e.preventDefault();
+    addtask(e.target.new_task_description_value);
+    todoList.reset();
+  });
 });
+
+function addtask(todoTasks) {
+  let li = document.createElement("li");
+  let btn = document.createElement("button");
+  btn.addEventListener("click", dltTask);
+  btn.textContent = "X";
+  li.textContent = `${todoTasks} `;
+  li.appendChild(btn);
+  document.querySelector("#tasks").appendChild(li);
+}
+
+function dltTask(e) {
+  e.target.parentNode.remove();
+}
+
